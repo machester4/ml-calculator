fcb07edcaacc3f87baa049c6b9aa725b1f7626a3
Michael Pintos <machester4@gmail.com>
2022-04-24 12:39:03 -0300

    [ADD] logs on publush msg

--- infra/messaging/pubsub/pubsub.go
+++ infra/messaging/pubsub/pubsub.go
@@ -2,6 +2,7 @@ package pubsubmessaging
 
 import (
 	"context"
+	"fmt"
 
 	"calculator.com/infra/messaging"
 	"cloud.google.com/go/pubsub"
@@ -15,9 +16,16 @@ var _ messaging.Messaging = (*pubsubMessaging)(nil)
 
 func (p pubsubMessaging) Publish(ctx context.Context, topic string, msg interface{}) {
 	topicName := p.client.Topic(topic)
-	topicName.Publish(ctx, &pubsub.Message{
+	res := topicName.Publish(ctx, &pubsub.Message{
 		Data: []byte(msg.(string)),
 	})
+
+	_, err := res.Get(ctx)
+	if err != nil {
+		fmt.Printf("Error publishing message: %v", err)
+	}
+
+	fmt.Printf("Published message: %v topic: %v", msg, topic)
 }
 
 func (p pubsubMessaging) Subscribe(ctx context.Context, topic string, handler messaging.MessageHandler) {
